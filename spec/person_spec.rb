929fc09d484a0c851bffab733bfca47e45d640ee
sami-p <sami.peterson14@gmail.com>
2021-06-03 10:15:46 -0500

    Test add_specialty method

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -21,4 +21,15 @@ RSpec.describe Person do
       expect(kathy.specialties).to eq([])
     end
   end
+
+  describe 'Person methods' do
+    it 'can add a speciality to specialties array' do
+      kathy = Person.new('Kathy Chan', 10)
+
+      kathy.add_specialty(:astrophysics)
+      kathy.add_specialty(:quantum_mechanics)
+
+      expect(kathy.specialties).to eq([:astrophysics, :quantum_mechanics])
+    end
+  end
 end
