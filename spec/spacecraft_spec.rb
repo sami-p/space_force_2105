bd3316c758341c64e1c104dd37d597ca94a8b602
sami-p <sami.peterson14@gmail.com>
2021-06-03 10:21:02 -0500

    Test attributes hash for Spacecraft instance

--- spec/spacecraft_spec.rb
+++ spec/spacecraft_spec.rb
@@ -8,5 +8,12 @@ RSpec.describe Spacecraft do
 
       expect(daedalus).to be_an_instance_of(Spacecraft)
     end
+
+    it 'has attributes' do
+      daedalus = Spacecraft.new({name: 'Daedalus', fuel: 400})
+
+      expect(daedalus.name).to eq('Daedalus')
+      expect(daedalus.fuel).to eq(400)
+    end
   end
 end
