e62d16d0af93292e2b9122a6cb7c3bcf915dce78
sami-p <sami.peterson14@gmail.com>
2021-06-03 10:13:03 -0500

    Add specialties instance variable and attr_reader

--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,11 @@
 class Person
   attr_reader :name,
-              :experience
+              :experience,
+              :specialties
 
   def initialize(name, experience)
     @name = name
     @experience = experience
+    @specialties = []
   end
 end
