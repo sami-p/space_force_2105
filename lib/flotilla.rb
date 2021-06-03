07489ed6b1652ed6ec02cf213a5f5f9285bbff3f
sami-p <sami.peterson14@gmail.com>
2021-06-03 12:00:00 -0500

    Last, unfinished attempt at recommend_personal with helper methods

--- lib/flotilla.rb
+++ lib/flotilla.rb
@@ -16,4 +16,26 @@ class Flotilla
   def add_personnel(person)
     @personnel << person
   end
+
+  def match_requirements(ship)
+    @personnel.find_all do |person|
+      person.specialties.each do |speciality|
+        ship.requirements.include?(speciality)
+      end
+    end
+  end
+
+  def experience_level(ship)
+    rec_personnel = []
+    match_requirements(ship).each do |person|
+      ship.requirements.any? { requirement.has_value? <= person.experience }
+        rec_personnel << person
+      end 
+    end
+    rec_personnel
+  end
+
+  def recommend_personnel(ship)
+    match_requirements(ship) && experience_level(ship)
+  end
 end
--- spec/flotilla_spec.rb
+++ spec/flotilla_spec.rb
@@ -64,5 +64,36 @@ RSpec.describe Flotilla do
 
       expect(seventh_flotilla.personnel).to eq([kathy, polly, rover, sampson])
     end
+
+    it 'can recommend personnel to a ship' do
+      kathy = Person.new('Kathy Chan', 10)
+      kathy.add_specialty(:astrophysics)
+      kathy.add_specialty(:quantum_mechanics)
+
+      polly = Person.new('Polly Parker', 8)
+      polly.add_specialty(:operations)
+      polly.add_specialty(:maintenance)
+
+      rover = Person.new('Rover Henriette', 1)
+      rover.add_specialty(:operations)
+      rover.add_specialty(:maintenance)
+
+      sampson = Person.new('Sampson Edwards', 7)
+      sampson.add_specialty(:astrophysics)
+      sampson.add_specialty(:quantum_mechanics)
+
+      daedalus = Spacecraft.new({name: 'Daedalus', fuel: 400})
+      daedalus.add_requirement({astrophysics: 6})
+      daedalus.add_requirement({quantum_mechanics: 3})
+
+      seventh_flotilla = Flotilla.new({designation: 'Seventh Flotilla'})
+      seventh_flotilla.add_ship(daedalus)
+      seventh_flotilla.add_personnel(kathy)
+      seventh_flotilla.add_personnel(polly)
+      seventh_flotilla.add_personnel(rover)
+      seventh_flotilla.add_personnel(sampson)
+
+      expect(seventh_flotilla.recommend_personnel(daedalus)). to eq([kathy, sampson])
+    end
   end
 end
