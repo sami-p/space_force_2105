c288e3d23e1f86d90f760cb702079184f78603d0
sami-p <sami.peterson14@gmail.com>
2021-06-03 10:10:34 -0500

    Test that instance of Person has attributes

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -7,5 +7,12 @@ RSpec.describe Person do
 
       expect(kathy).to be_an_instance_of(Person)
     end
+
+    it 'has attributes' do
+      kathy = Person.new('Kathy Chan', 10)
+
+      expect(kathy.name).to eq('Kathy Chan')
+      expect(kathy.experience).to eq(10)
+    end
   end
 end
